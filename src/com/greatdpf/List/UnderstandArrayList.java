03b6af15eb46091b1f131f4619d2b780a7910048
dpf <greatdpf@163.com>
2020-10-26 19:48:44 +0800

    深入刨析 Java 集合 ArrayList 1.8 和 1.7 源码

--- src/com/greatdpf/List/UnderstandArrayList.java
+++ src/com/greatdpf/List/UnderstandArrayList.java
@@ -1,5 +1,7 @@
 package com.greatdpf.List;
 
+import java.util.ArrayList;
+
 /**
  * 理解 Java 集合中 ArrayList
  * ArrayList 源码1.7和1.8不同！
@@ -13,6 +15,15 @@ public class UnderstandArrayList {
      * 程序的入口
      */
     public static void main(String[] args){
+        /*
+        ArrayList<E>
+        类型：Class 类
+        继承：AbstractList<E> 抽象类
+        实现：
+         <----------------------特有方法没有重要的，直接看源码------------------------>
+
+          */
+        // 方法与 List 差不多，不演示
 
     }
 }
